f5e1980065c2d4948300dfdd41214ad273cc36f4
Aman <amanvrma5799@gmail.com>
2024-11-10 21:43:34 +0530

    Update main return condition

--- app/services/gdbmi.py
+++ app/services/gdbmi.py
@@ -11,7 +11,7 @@ def get_trace_step(gdb_controller, gdb_command):
     step = {'function': '', 'line': -1, 'stack_frames': [], 'heap': {}, 'stdout': ''}
 
     results = gdb_controller.write(gdb_command)
-    if results[-1]['payload'].get('frame') and results[-1]['payload']['frame']['func'] == '__libc_start_call_main':
+    if results[-1]['payload'].get('frame') and results[-1]['payload']['frame']['func'] == '__libc_start_main':
         return 'main_returned'
     
     stdout = next((res['payload'] for res in results if res['type'] == 'output'), '')
